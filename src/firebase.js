b1eb9edb11a262bbbba65085fa15bda679f822c5
Alfredoeb9 <alfredoeb96@gmail.com>
2020-09-05 12:03:28 -0700

    updated files & connected firebase for hosting and auth.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBvD_65y5gFQn4Cp_SpMiCFFi1XYp1m7XU",
+  authDomain: "whatsapp-clone-5ac47.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-5ac47.firebaseio.com",
+  projectId: "whatsapp-clone-5ac47",
+  storageBucket: "whatsapp-clone-5ac47.appspot.com",
+  messagingSenderId: "554435107285",
+  appId: "1:554435107285:web:19df670295d8a423cac55c",
+  measurementId: "G-XJSLX9HLD1"
+};
