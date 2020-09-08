2499df2a7af9ddb5ee6f6cb9afbaee6c8e45f02b
Alfredoeb9 <alfredoeb96@gmail.com>
2020-09-07 21:39:33 -0700

    updated chat component added icons and chat body

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import './Chat.css';
+import { Avatar, IconButton } from '@material-ui/core';
+import { SearchOutlined, AttachFile } from '@material-ui/icons';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Chat() {
   return (
     <div className="chat">
-      <h1>I am the chat</h1>
+      <div className="chat_header">
+        <Avatar />
+
+        <div className="chat_headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+
+        <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   )
 }
